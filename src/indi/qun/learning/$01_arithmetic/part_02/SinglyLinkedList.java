83b6f050c5faa5d2e500c4c781c9dc1c0f872478
yq <v_yangguanqun@100tal.com>
2022-02-06 15:54:55 +0800

    add MySinglyLinkedList

--- src/indi/qun/learning/$01_arithmetic/part_02/SinglyLinkedList.java
+++ src/indi/qun/learning/$01_arithmetic/part_02/SinglyLinkedList.java
@@ -1,26 +1,144 @@
 package indi.qun.learning.$01_arithmetic.part_02;
 
 
+
 /**
  * 学习目标:
  * 1.Java实现一个单链表，Java当中是没有单链表的
  * 2.单链表反转
  * 3.链表中环的检测  -1) 快慢指针  2)hashMap找重
  * 4.两个有序链表合并
+ * 5.删除链表倒数第n个节点 [deleteNode1]自己写成了删除倒数几个节点
+ * 6.求单链表的中间节点    -1)快慢指针
  */
 public class SinglyLinkedList {
     public static void main(String[] args) {
-        var mySingleLinkedList = new MySingleLinkedList();
+        MySingleLinkedList mySingleLinkedList = new MySingleLinkedList();
         mySingleLinkedList.add(1);
         mySingleLinkedList.add(2);
         mySingleLinkedList.add(3);
-        System.out.println(mySingleLinkedList);
-        MySingleLinkedList.Node reverse = MySingleLinkedList.reverse(mySingleLinkedList.head);
-        System.out.println(new MySingleLinkedList(reverse));
+        mySingleLinkedList.add(4);
+        mySingleLinkedList.add(5);
+        System.out.println("原始链表内容:" + mySingleLinkedList);
+        // 单链表反转
+//        MySingleLinkedList.Node reverse = MySingleLinkedList.reverse(mySingleLinkedList.head);
+//        System.out.println(new MySingleLinkedList(reverse));
+//        MySingleLinkedList mySingleLinkedList1 = new MySingleLinkedList();
+//        mySingleLinkedList1.add(2);
+//        mySingleLinkedList1.add(4);
+        // 两个有序链表合并
+//        MySingleLinkedList.Node mergeTwoLists =  MySingleLinkedList.mergeTwoLists(mySingleLinkedList.head,mySingleLinkedList1.head);
+//        System.out.println(new MySingleLinkedList(mergeTwoLists));
+        // 删除链表倒数n个节点
+//        MySingleLinkedList.Node deleteLocateNode = MySingleLinkedList.deleteLocateNode(mySingleLinkedList.head, 3);
+        // 删除链表倒数第n个节点
+//        MySingleLinkedList.Node deleteLocateNode = MySingleLinkedList.deleteNode1(mySingleLinkedList.head, 3);
+//        System.out.println(new MySingleLinkedList(deleteLocateNode));
+        // 求链表的中间节点
+        MySingleLinkedList.Node locateMiddle = MySingleLinkedList.locateMiddle(mySingleLinkedList.head);
+        System.out.println(new MySingleLinkedList(locateMiddle));
     }
+
 
     public static class MySingleLinkedList {
         private Node head = null;
+
+        public static Node locateMiddle(Node list) {
+            Node fast = list, slow = list;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return new Node(slow.data);
+        }
+
+        public static Node deleteLocateNode(Node list, int n) {
+            Node locateNode = MySingleLinkedList.locateNode(list, n);
+            Node result = new Node(0, list);
+            Node q = result;
+            int i = 0;
+            while (list != null) {
+                if (list == locateNode) {
+                    break;
+                } else {
+                    list = list.next;
+                    i++;
+                }
+            }
+            while (i > 0) {
+                i--;
+                q = q.next;
+            }
+
+            q.next = null;
+            return result.next;
+        }
+
+        public static Node locateNode(Node list, int n) {
+            Node fast = list, slow = list;
+            while (n > 0) {
+                fast = fast.next;
+                n--;
+            }
+            while (fast != null && slow != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+
+        /**
+         * 删除链表的第n个节点
+         * @param list  链表
+         * @param n     倒数第n
+         * @return Node 返回删除后的新链表
+         */
+        public static Node deleteNode1(Node list, int n) {
+            Node fast = list, slow = list,result=null;
+            while (n > 0) {
+                fast = fast.next;
+                n--;
+            }
+            while (fast != null && slow != null) {
+                fast = fast.next;
+                result = slow;
+                slow = slow.next;
+            }
+            if(result != null){
+                result.next = result.next.next;
+            }
+            return list;
+        }
+        /**
+         * 合并两个有序单链表
+         *
+         * @param l1
+         * @param l2
+         * @return
+         */
+        public static Node mergeTwoLists(Node l1, Node l2) {
+            Node soldier = new Node(0); //利用哨兵结点简化实现难度 技巧三
+            Node p = soldier;
+
+            while (l1 != null && l2 != null) {
+                if (l1.data < l2.data) {
+                    p.next = l1;
+                    l1 = l1.next;
+                } else {
+                    p.next = l2;
+                    l2 = l2.next;
+                }
+                p = p.next;
+            }
+            // 检查末尾一个值的情况
+            if (l1 != null) {
+                p.next = l1;
+            }
+            if (l2 != null) {
+                p.next = l2;
+            }
+            return soldier.next;
+        }
 
         public MySingleLinkedList() {
         }
@@ -29,22 +147,29 @@ public class SinglyLinkedList {
             this.head = head;
         }
 
-        public boolean checkCycle(Node list){
+        /**
+         * 检测链表中是否有环
+         *
+         * @param list 单链表
+         * @return boolean 是否有环
+         */
+        public static boolean checkCycle(Node list) {
             boolean result = false;
-            if(list == null){
+            if (list == null) {
                 return result;
             }
             Node fast = list.next;
             Node slow = list;
-            while(fast !=null && fast.next !=null){
+            while (fast != null && fast.next != null) {
                 fast = fast.next.next;
                 slow = slow.next;
-                if(slow == fast){
+                if (slow == fast) {
                     result = true;
                 }
             }
             return result;
         }
+
         /**
          * 单链表反转
          *
@@ -94,6 +219,9 @@ public class SinglyLinkedList {
             StringBuilder sb = new StringBuilder();
             Node q = head;
             sb.append(q.data).append(",");
+            if(null == q.next){
+                return sb.toString();
+            }
             do {
                 q = q.next;
                 sb.append(q.data).append(",");
@@ -131,6 +259,13 @@ public class SinglyLinkedList {
                 this.next = next;
             }
 
+            @Override
+            public String toString() {
+                return "Node{" +
+                        "next=" + next +
+                        ", data=" + data +
+                        '}';
+            }
         }
 
     }
