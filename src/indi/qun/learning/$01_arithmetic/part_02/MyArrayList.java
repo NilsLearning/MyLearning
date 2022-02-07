c5c5158b79c7c4ac44c3e6673d56b7e76fccd770
yangqun <qun.yang@non.se.com>
2022-02-07 10:07:06 +0800

    temp

---
+++ src/indi/qun/learning/$01_arithmetic/part_02/MyArrayList.java
@@ -0,0 +1,9 @@
+package indi.qun.learning.$01_arithmetic.part_02;
+
+import java.util.ArrayList;
+
+public class MyArrayList {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+    }
+}
