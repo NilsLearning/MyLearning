b8d5be7bf832080ea83a8d4ac98a56584b559ab5
yq <v_yangguanqun@100tal.com>
2022-02-11 17:01:50 +0800

    add sort

--- src/indi/qun/learning/$01_arithmetic/part_02/MySort.java
+++ src/indi/qun/learning/$01_arithmetic/part_02/MySort.java
@@ -1,6 +1,8 @@
 package indi.qun.learning.$01_arithmetic.part_02;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 记录下学习的排序算法代码
@@ -8,12 +10,16 @@ import java.util.Arrays;
 public class MySort {
     public static void main(String[] args) {
         MySort mySort = new MySort();
-        int[] numbers = new int[]{1, 2, 0};
+        int[] numbers = new int[]{2, 1};
 //        mySort.bubbleSort(numbers);
-        mySort.insertionSortFromWangZheng(numbers);
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
+//        mySort.insertionSortFromWangZheng(numbers);
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println(numbers[i]);
+//        }
+
+        mySort.mergeSort(numbers);
+        System.out.println(numbers);
+
     }
 
     /**
@@ -105,4 +111,58 @@ public class MySort {
         }
     }
 
+    public void mergeSort(int[] numbers){
+        mergeSortSon(numbers,0,numbers.length-1);
+    }
+
+
+    /**
+     * 归并排序
+     * @param numbers
+     */
+    public void mergeSortSon(int[] numbers,int startIndex,int endIndex) {
+        if (startIndex >= endIndex) return;
+        int middle = (startIndex+endIndex)/2;
+        mergeSortSon(numbers,startIndex,middle);
+        mergeSortSon(numbers,middle+1,endIndex);
+//        merge(numbers,numbers.subList(startIndex,middle),numbers.subList(middle+1,endIndex));
+        merge(numbers, startIndex, middle, endIndex);
+    }
+
+//    public void merge(List<Integer> numbers, List<Integer> leftIndex, List<Integer> rightIndex) {
+////        int i
+//
+//    }
+
+    private  void merge(int[] a, int p, int q, int r) {
+        int i = p;
+        int j = q+1;
+        int k = 0; // 初始化变量i, j, k
+        int[] tmp = new int[r-p+1]; // 申请一个大小跟a[p...r]一样的临时数组
+        while (i<=q && j<=r) {
+            if (a[i] <= a[j]) {
+                tmp[k++] = a[i++]; // i++等于i:=i+1
+            } else {
+                tmp[k++] = a[j++];
+            }
+        }
+
+        // 判断哪个子数组中有剩余的数据
+        int start = i;
+        int end = q;
+        if (j <= r) {
+            start = j;
+            end = r;
+        }
+
+        // 将剩余的数据拷贝到临时数组tmp
+        while (start <= end) {
+            tmp[k++] = a[start++];
+        }
+
+        // 将tmp中的数组拷贝回a[p...r]
+        for (i = 0; i <= r-p; ++i) {
+            a[p+i] = tmp[i];
+        }
+    }
 }
